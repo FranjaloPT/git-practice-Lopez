90ac2ac6743780b200548b0bcb54bad9896d397b
FranjaloPT <franjalo0928@gmail.com>
2026-01-29 12:08:47 -0400

    Update hola_mundo.cpp

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -6,6 +6,9 @@ int main(){
 	cout<<"Mi nombre es Francisco Lopez"<<endl;
 	cout<<"Estoy aprendiendo Git en programacion II"<<endl;
 	cout<<"Este es mi segundo commit"<<endl;
+	cout<<"Ahora me movi a Github"<<endl;
+	/*Me salté el Hack 3 porque no pude con eso, no lo entendí. Los demás me han costado algo
+	Pero me he guiado con videos de Youtube y tutoriales en el navegador. Espero me ayuden*/
 	return 0;
 	
 }
