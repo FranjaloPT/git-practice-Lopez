951caf03591bdf75e6892aa4a37fca079ee2b2cb
Francisco Lopez <franjalo0928@gmail.com>
2026-01-29 10:48:19 -0400

    Feta: agregar mas mensajes al programa

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(){
 	cout<<"Hola mundo desde Git!"<<endl;
 	cout<<"Mi nombre es Francisco Lopez"<<endl;
+	cout<<"Estoy aprendiendo Git en programacion II"<<endl;
+	cout<<"Este es mi segundo commit"<<endl;
 	return 0;
 	
 }
